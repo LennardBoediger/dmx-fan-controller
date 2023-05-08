c3e0934223f250735a784e342ad97fc0a8860eef
lennard.boediger <lennard@nitrokey.com>
2023-05-08 20:50:39 +0200

    add code for basic speed control

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,12 +1,47 @@
 #include <Arduino.h>
 
+#define DAC_MAX_VALE 1024*4-1
+
+// SPI Pins for DAC
+const int CSPin = 4;
+const int SCKPin = 3;
+const int MOSIPin = 2;
+
+// This function transmits data via SPI to the DAC (MCP4922)
+// WRITE COMMAND REGISTER FOR MCP4922 (12-BIT DAC):
+// Bit 15: channel (A=0, B=1), 
+// Bit 14: Buffer (default=0), 
+// Bit 13: Gain (1x=1, 2x=0), 
+// Bit 12: shutdown (active=1. inactive-0),
+// Bit 11-0: DAC Input Data bits.
+// p. 24
+void setDac(unsigned long value){ 
+  unsigned long dacRegister = 0b0011000000000000;
+  // Set channel A
+  dacRegister &= ~(1 << 15);
+  // Generate the two bytes that will be sent to the DAC
+  unsigned long dacSendBytes = dacRegister | value;
+
+  // Start SPI transmission
+  digitalWrite(CSPin, LOW); 
+  for(int i = 0; i < 16; i++){
+    digitalWrite(MOSIPin, bitRead(dacSendBytes, 15-i));
+    delay(2);
+    digitalWrite(SCKPin, HIGH);
+    delay(2);
+    digitalWrite(SCKPin, LOW);
+  }
+  digitalWrite(CSPin, HIGH);
+}
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(CSPin, OUTPUT);
+  pinMode(SCKPin, OUTPUT);
+  pinMode(MOSIPin, OUTPUT);
+  digitalWrite(CSPin, HIGH);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+  setDac(DAC_MAX_VALE);
+  delay(4);
 }
