0508eb2be86527605edd77008fdefe24b0370900
lennard.boediger <lennard@nitrokey.com>
2023-05-10 13:21:22 +0200

    Add DMX functionality (not tested)

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,11 +1,26 @@
 #include <Arduino.h>
+#include <DMXSerial.h>
 
-#define DAC_MAX_VALE 1024*4-1
+#define DAC_MAX_VALUE 4095
+#define DAC_MIN_VALUE 0
+#define DMX_MAX_VALUE 255
 
 // SPI Pins for DAC
 const int CSPin = 4;
 const int SCKPin = 3;
 const int MOSIPin = 2;
+
+// DIP switch pins for DMX address 
+const int DMX0 = 6;
+const int DMX1 = 7;
+const int DMX2 = 8;
+const int DMX3 = 9;
+const int DMX4 = 10;
+const int DMX5 = 11;
+const int DMX6 = 12;
+const int DMX7 = 16;
+const int DMX8 = 15;
+const int DMXfunc = 14;
 
 // This function transmits data via SPI to the DAC (MCP4922)
 // WRITE COMMAND REGISTER FOR MCP4922 (12-BIT DAC):
@@ -35,13 +50,26 @@ void setDac(unsigned long value){
 }
 
 void setup() {
+  // SPI
   pinMode(CSPin, OUTPUT);
   pinMode(SCKPin, OUTPUT);
   pinMode(MOSIPin, OUTPUT);
   digitalWrite(CSPin, HIGH);
+  //DMX
+  pinMode(DMX0, INPUT);
+  pinMode(DMX1, INPUT);
+  pinMode(DMX2, INPUT);
+  pinMode(DMX3, INPUT);
+  pinMode(DMX4, INPUT);
+  pinMode(DMX5, INPUT);
+  pinMode(DMX6, INPUT);
+  pinMode(DMX7, INPUT);
+  pinMode(DMX8, INPUT);
+  DMXSerial.init(DMXReceiver);
 }
 
 void loop() {
-  setDac(DAC_MAX_VALE);
+  uint16_t DMXChannel = !digitalRead(DMX0) + (!digitalRead(DMX1) << 1) + (!digitalRead(DMX2) << 2) + (!digitalRead(DMX3) << 3) + (!digitalRead(DMX4) << 4) + (!digitalRead(DMX5) << 5) + (!digitalRead(DMX6) << 6)+ (!digitalRead(DMX7) << 7) + (!digitalRead(DMX8) << 8);
+  setDac((DAC_MAX_VALUE / DMX_MAX_VALUE) * DMXSerial.read(DMXChannel));
   delay(4);
-}
+} 
